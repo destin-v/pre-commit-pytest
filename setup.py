0cb781cd75a7450fcd19da7e7b58cf7612b0204b
destin-v <47424506+destin-v@users.noreply.github.com>
2021-02-10 19:50:16 -0500

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,9 @@
+from setuptools import find_packages
+from setuptools import setup
+
+setup(
+    name='Pre-commit Pytest Setup File',
+    version='1.0',
+    packages=find_packages(),
+    install_requires=['pytest']
+    )
